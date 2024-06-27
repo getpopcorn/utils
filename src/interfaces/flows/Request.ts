8fec9a06028c830e11dd9028e5c8347ce471eb47
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2024-06-27 14:56:47 +0200

    fix: remove Buffer from request type

--- src/interfaces/flows/Request.ts
+++ src/interfaces/flows/Request.ts
@@ -8,5 +8,5 @@ export type RequestInput = {
 };
 
 export type RequestMakeInput = {
-  message?: Record<string, any> | string | Buffer | ArrayBuffer | Uint8Array;
+  message?: Record<string, any> | string | ArrayBuffer | Uint8Array;
 };
